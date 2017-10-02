bd15d2d3a7786be4dfb3df30e334fa39afc9907b
Martin Sherene <emes001@gmail.com>
2017-10-02 15:34:20 -0400

    Initial commit for video dashboard

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const dashboardRoutes: Routes = [
+  { path: '', component: VideoDashboardComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(dashboardRoutes)
+  ],
+  exports: [RouterModule],
+  declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent]
+})
+export class DashboardModule { }
